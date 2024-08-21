2aa1504adbae5a7a7e16ef01844e0bee6e094ab0
Kodai-Java <kodai8250@gmail.com>
2024-08-22 01:55:56 +0900

    create: spring boot

---
+++ src/main/java/com/kodai/practice_springboot_todo/PracticeSpringbootTodoApplication.java
@@ -0,0 +1,13 @@
+package com.kodai.practice_springboot_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringbootTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodai/practice_springboot_todo/PracticeSpringbootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodai.practice_springboot_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringbootTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
